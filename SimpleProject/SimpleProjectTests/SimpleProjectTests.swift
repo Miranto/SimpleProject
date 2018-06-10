867b097e0d917af2d1149763211e6a825350cb7e
Mateusz Mirkowski <>
2018-06-10 14:22:00 +0200

    Add basic tests

--- SimpleProject/SimpleProjectTests/SimpleProjectTests.swift
+++ SimpleProject/SimpleProjectTests/SimpleProjectTests.swift
@@ -10,27 +10,40 @@ import XCTest
 @testable import SimpleProject
 
 class SimpleProjectTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  
+  let photosViewModel = PhotosViewModel()
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    photosViewModel.currentPhotoId = 1
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func testSettingCurrentPhotoId() {
+    XCTAssertEqual(photosViewModel.currentPhotoId, 1)
+  }
+  
+  func testResetCurrentPhotoId() {
+    photosViewModel.resetData()
+    XCTAssertEqual(photosViewModel.currentPhotoId, 0)
+  }
+  
+  func testResetPhotoData() {
+    photosViewModel.resetData()
+    XCTAssertEqual(photosViewModel.photosData.value.count, 0)
+  }
+  
+  func testPerformanceExample() {
+    // This is an example of a performance test case.
+    self.measure {
+      // Put the code you want to measure the time of here.
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+  }
+  
 }
+
